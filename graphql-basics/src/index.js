eebba28366c315d9a9872af3c8e6c98a235dd768
André Sebastian <a.sebastian@posteo.de>
2020-07-19 12:35:43 +0200

    add basic graphql server

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,17 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello () {
+            return 'This is my first query!'
+        }
+    }
+}
