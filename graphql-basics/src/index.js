fe69604f497ee906d2ef0f74a2488975c7c44e22
André Sebastian <a.sebastian@posteo.de>
2020-07-24 21:05:13 +0200

    finish section 2

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -51,22 +51,26 @@ const comments = [
     {
         id: "111",
         text: "What a nice comment!",
-        author: "1"
+        author: "1",
+        post: "10"
     },
     {
         id: "222",
         text: "What a great comment!",
-        author: "2"
+        author: "2",
+        post: "20"
     },
     {
         id: "333",
         text: "What a crazy comment!",
-        author: "3"
+        author: "3",
+        post: "30"
     },
     {
         id: "444",
         text: "What a strange comment!",
-        author: "2"
+        author: "2",
+        post: "30"
     }
 ]
 
@@ -94,12 +98,14 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
 
     type Comment {
         id: ID!
         text: String!
         author: User!
+        post: Post!
     }
 `
 
@@ -149,6 +155,11 @@ const resolvers = {
             return users.find((user)=> {
                 return user.id === parent.author
             })
+        },
+        comments (parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
+            })
         }
     },
     User: {
@@ -159,7 +170,7 @@ const resolvers = {
         },
         comments (parent, args, ctx, info) {
             return comments.filter((comment) => {
-                return comment.author === parent.id
+                return comment.post === parent.post
             })
         }
         
@@ -168,6 +179,11 @@ const resolvers = {
         author (parent, args, ctx, info) {
             return users.find((user) => {
                 return user.id === parent.author
+            })
+        },
+        post (parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
             })
         }
     }
