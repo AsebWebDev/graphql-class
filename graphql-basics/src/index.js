3c408806df45c146e59074116be89aea5d15088f
André Sebastian <a.sebastian@posteo.de>
2020-07-25 11:25:06 +0200

    finish chapter 26

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga'
+import { v4 as uuidv4 } from 'uuid';
 
 // Scalar types - String, Boolean, Int, Float, ID
 
@@ -83,6 +84,12 @@ const typeDefs = `
         comments(query: String): [Comment!]!
     }
 
+    type Mutation {
+        createUser(name: String!, email: String!, age: Int) : User!
+        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+        createComment(text: String!, author: ID!, post: ID!): Comment!
+    }
+
     type User {
         id: ID!
         name: String!
@@ -148,6 +155,43 @@ const resolvers = {
             return comments.filter((comment) => {
                 return comment.text.toLowerCase().includes(args.query.toLowerCase())
             })
+        }
+    },
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+            const emailTaken = users.some((user) => user.email === args.email)
+            if ( emailTaken ) throw new Error('Email taken')
+            const user = {
+                id: uuidv4(),
+                ...args
+            }
+
+            users.push(user)
+            return user
+        },
+        createPost(parent, args, ctx, info) {
+            const userExists = users.some((user) => user.id === args.author);
+            if ( !userExists ) throw new Error('User does not exists');
+            const post = {
+                id: uuidv4(),
+                ...args
+            }
+
+            posts.push(post)
+            return post
+        },
+        createComment(parent, args, ctx, info) {
+            const userExists = users.some((user) => user.id === args.author);
+            const postExists = posts.find((post) => (post.id === args.post && post.published));
+            if ( !userExists ) throw new Error('User does not exists');
+            if ( !postExists ) throw new Error('Post does not exists');
+            const comment = { 
+                id: uuidv4(), 
+                ...args 
+            }
+
+            comments.push(comment)
+            return comment
         }
     },
     Post: {
