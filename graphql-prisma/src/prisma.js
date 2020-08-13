3cd993127e197f179e10e2638306349226282454
André Sebastian <a.sebastian@posteo.de>
2020-08-13 08:43:00 +0200

    play bindings + mutations

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,10 +5,43 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 }) 
 
-prisma.query.users(null, '{ id name posts { id title } }')
-    .then((data) => console.log(JSON.stringify(data, null, 2)))
-    .catch((error) => console.log(error))
 
-prisma.query.comments(null, '{ id text author { id name } }')
+// prisma.query.comments(null, '{ id text author { id name } }')
+//     .then((data) => console.log(JSON.stringify(data, null, 2)))
+//     .catch((error) => console.log(error))
+
+// prisma.mutation.createPost({
+//     data: {
+//         title: 'My new post 2000',
+//         body: 'You can find the course here', 
+//         published: true, 
+//         author: {
+//             connect: {
+//                 id: 'ckdls6bj4000u0875wuatgllg'
+//             }
+//         }
+//     }
+// }, `{
+//     id title body published
+// }`)
+//     .then((data) => {
+//         console.log(JSON.stringify(data, null, 2))
+//         return prisma.query.users(null, '{ id name posts { id title } }')
+//     })
+//     .then((data) => console.log(JSON.stringify(data, null, 2)))
+//     .catch((error) => console.log(error))
+
+prisma.mutation.updatePost({
+    data: {
+        published: false, 
+        title: 'My new posts 2001'
+    },
+    where: {
+        id: 'ckdsffsie00110875ojmmh207'
+    }
+}, `{
+    id title body published
+}`)
+    .then((data) => prisma.query.users(null, '{ id name posts { id title } }'))
     .then((data) => console.log(JSON.stringify(data, null, 2)))
     .catch((error) => console.log(error))
