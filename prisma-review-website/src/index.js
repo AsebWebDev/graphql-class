da02e9346c1fa032cd4b796dd81941c7610ab113
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-18 08:16:52 +0200

    add new project review website

---
+++ prisma-review-website/src/index.js
@@ -0,0 +1,19 @@
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+import db from './db';
+import Query from './Resolvers/Query'
+import Mutation from './Resolvers/Mutation'
+import User from './Resolvers/User'
+import Post from './Resolvers/Post'
+import Comment from './Resolvers/Comment'
+import Subscription from './Resolvers/Subscription'
+import './prisma.js'
+
+const pubsub = new PubSub()
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers : { Query, Mutation, User, Post, Comment, Subscription },
+    context: { db, pubsub }
+})
+
+server.start(() => { console.log('The server is up!') })
